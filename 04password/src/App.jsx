f5b9d78102fa4a768444d880835abc4a94aec343
Godhani <bhaumikgodgani150@gmail.com>
2024-06-07 17:27:21 +0530

    second updata

--- 04password/src/App.jsx
+++ 04password/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         className="w-full max-w-md mx-auto shadow-md rounded-lg
    px-4 py-8 text-orange-500 bg-gray-800">
         <h1 className="text-white text-center text-center my-3">
-          Password_Generate
+        Password Generator 
         </h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
@@ -78,7 +78,7 @@ function App() {
           />
           <label>Length: {length}</label>
         </div>
-        <div className="flex items-center gap-x-1">
+        {/* <div className="flex items-center gap-x-1">
           <input
             type="checkbox"
             defaultChecked={number}
@@ -88,7 +88,37 @@ function App() {
             }}
           />
           <label htmlFor="numberInput">Number</label>
+        </div> */}
+
+
+
+
+
+
+<br/>
+       
+<h3 class="mb-4 font-semibold text-gray-900 dark:text-white"></h3>
+<ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+        <div class="flex items-center ps-3">
+            <input onClick={()=>{
+              setnumber((prev)=> !prev);
+            }} id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+            <label for="vue-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Number</label>
         </div>
+        <div class="flex items-center ps-3">
+            <input onClick={()=>{
+              setchar((prev)=> !prev); }}id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+            <label for="vue-checkbox" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Character</label>
+        </div>
+    </li>
+  
+</ul>
+
+
+
+
+{/* 
         <div className="flex items-center gap-x-1">
           <input
             type="checkbox"
@@ -99,7 +129,7 @@ function App() {
             }}
           />
           <label htmlFor="characterInput">Character</label>
-        </div>
+        </div> */}
       </div>
     </>
   );
