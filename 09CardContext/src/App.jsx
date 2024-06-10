0046836a9757db1a93215fd568a8e4dd0d3d19ca
Godhani <bhaumikgodgani150@gmail.com>
2024-06-10 15:12:41 +0530

    theme color change in using context

---
+++ 09CardContext/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setthemode] = useState("light");
+  // const [darkTheme, setdarkTheme] = useState("light")
+  // const [lightTheme, setlightTheme] = useState("light")
+
+  const lightTheme = () => {
+    setthemode("light");
+  };
+  const darkTheme = () => {
+    setthemode("dark");
+  };
+
+  // change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <ThemeBtn/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto"><Card/></div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
